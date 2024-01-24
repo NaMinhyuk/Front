669dceb801654ebd79b1411d52342f33c39874e6
songhayeong <sosscvs@gmail.com>
2024-01-24 19:50:23 +0900

    fixed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,12 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // For control over item selection of both touch and mouse driven selection
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
---
+++ app/src/main/java/com/example/lifesharing/chat/ChatRoomItem.kt
@@ -0,0 +1,12 @@
+package com.example.lifesharing.chat
+
+data class ChatRoomItem(
+
+    // username, profileUrl, chattime, ItemImageUrl
+    var username: String? = null,
+    var location: String? = null,
+    var profile: String? = null,
+    var chattime: String? = null,
+    var itemImageUrl: String? = null
+
+)
