4fddf1d9949344e86761b965c3596443082b950d
songhayeong <sosscvs@gmail.com>
2024-02-09 03:29:36 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    val kotlin_version by extra("2.0.0-Beta3")
     dependencies {
         classpath("com.google.gms:google-services:4.3.13")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+    repositories {
+        mavenCentral()
     }
 }
 
